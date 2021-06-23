e36fc36c23acc2361cff7db63fc9b3cf58ffc0f3
张亚飞 <672233580@qq.com>
2021-06-23 14:42:54 +0800

    Initial Commit

---
+++ WalletUI/WalletUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletUIApp.swift
+//  WalletUI
+//
+//  Created by 张亚飞 on 2021/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WalletUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
